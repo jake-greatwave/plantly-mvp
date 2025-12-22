9d1086697b46f951a2eaf5e466bf605483d35e71
jake-greatwave <jake@greatwave.co.kr>
2025-12-22 12:33:03 +0900

    [feat] 어드민 기능 추가

--- lib/types/database.types.ts
+++ lib/types/database.types.ts
@@ -18,6 +18,7 @@ export type Database = {
           phone: string | null;
           status: "active" | "suspended";
           user_grade: "basic" | "enterprise";
+          is_admin: boolean;
           created_at: string;
           updated_at: string;
         };
@@ -29,6 +30,7 @@ export type Database = {
           phone?: string | null;
           status?: "active" | "suspended";
           user_grade?: "basic" | "enterprise";
+          is_admin?: boolean;
           created_at?: string;
           updated_at?: string;
         };
@@ -40,6 +42,7 @@ export type Database = {
           phone?: string | null;
           status?: "active" | "suspended";
           user_grade?: "basic" | "enterprise";
+          is_admin?: boolean;
           created_at?: string;
           updated_at?: string;
         };
